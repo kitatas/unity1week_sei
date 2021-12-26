2159b2a1319bfd803708191e9e9f47a51504351a
kitatas <315kitatas10703@gmail.com>
2021-12-26 18:44:49 +0900

    add: ステージのアウトライン回転

--- Assets/Sei/Scripts/Main/Presentation/Controller/FieldController.cs
+++ Assets/Sei/Scripts/Main/Presentation/Controller/FieldController.cs
@@ -1,9 +1,24 @@
+using DG.Tweening;
+using MPUIKIT;
 using UnityEngine;
 
 namespace Sei.Main.Presentation.Controller
 {
     public sealed class FieldController : MonoBehaviour
     {
-        
+        [SerializeField] private MPImage field = default;
+        private static readonly int _gradientRotation = Shader.PropertyToID("_GradientRotation");
+
+        private void Start()
+        {
+            DOTween.Sequence()
+                .Append(DOTween.To(
+                    () => field.material.GetFloat(_gradientRotation),
+                    value => field.material.SetFloat(_gradientRotation, value),
+                    360.0f,
+                    2.0f))
+                .SetLoops(-1, LoopType.Restart)
+                .SetLink(gameObject);
+        }
     }
 }
