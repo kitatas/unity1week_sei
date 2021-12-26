c120da7a73c1c17041774699b3677f109ceccabf
kitatas <315kitatas10703@gmail.com>
2021-12-26 18:45:52 +0900

    fix: 警告が表示されないように

--- Assets/Sei/Scripts/Main/Presentation/Controller/State/BaseGameState.cs
+++ Assets/Sei/Scripts/Main/Presentation/Controller/State/BaseGameState.cs
@@ -7,12 +7,16 @@ namespace Sei.Main.Presentation.Controller
     {
         public abstract GameState state { get; }
 
+#pragma warning disable CS1998
         public virtual async UniTask InitAsync(CancellationToken token)
+#pragma warning restore CS1998
         {
 
         }
 
+#pragma warning disable CS1998
         public virtual async UniTask<GameState> TickAsync(CancellationToken token)
+#pragma warning restore CS1998
         {
             return GameState.None;
         }
