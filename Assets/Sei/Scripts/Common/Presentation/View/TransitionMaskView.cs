fe1b746233101613e02434c1d61ee9d48d346da5
kitatas <315kitatas10703@gmail.com>
2021-12-26 13:52:39 +0900

    fix: Transition

--- Assets/Sei/Scripts/Common/Presentation/View/TransitionMaskView.cs
+++ Assets/Sei/Scripts/Common/Presentation/View/TransitionMaskView.cs
@@ -1,32 +1,62 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using MPUIKIT;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Sei.Common.Presentation.View
 {
     public sealed class TransitionMaskView : MonoBehaviour
     {
-        [SerializeField] private Image mask = default;
+        [SerializeField] private MPImage mask = default;
+        private static readonly int _radius = Shader.PropertyToID("_CircleRadius");
+        private readonly float _hide = 380.0f;
+        private readonly float _show = 100.0f;
 
         public void Init()
         {
+            DOTween.To(
+                () => mask.material.GetFloat(_radius),
+                x => mask.material.SetFloat(_radius, x),
+                _show,
+                0.0f);
             mask.DOFade(0.0f, 0.0f);
+            mask.raycastTarget = false;
         }
 
         public async UniTask FadeInAsync(CancellationToken token)
         {
-            await mask
-                .DOFade(1.0f, CommonConfig.TRANSITION_TIME)
-                .WithCancellation(token);
+            mask.raycastTarget = true;
+            await (
+                DOTween.To(
+                        () => mask.material.GetFloat(_radius),
+                        x => mask.material.SetFloat(_radius, x),
+                        _hide,
+                        CommonConfig.TRANSITION_TIME)
+                    .SetLink(gameObject)
+                    .WithCancellation(token),
+                mask.DOFade(1.0f, CommonConfig.TRANSITION_TIME)
+                    .SetLink(gameObject)
+                    .WithCancellation(token)
+            );
         }
 
         public async UniTask FadeOutAsync(CancellationToken token)
         {
-            await mask
-                .DOFade(0.0f, CommonConfig.TRANSITION_TIME)
-                .WithCancellation(token);
+            await (
+                DOTween.To(
+                        () => mask.material.GetFloat(_radius),
+                        x => mask.material.SetFloat(_radius, x),
+                        _show,
+                        CommonConfig.TRANSITION_TIME)
+                    .SetLink(gameObject)
+                    .WithCancellation(token),
+                mask.DOFade(0.0f, CommonConfig.TRANSITION_TIME)
+                    .SetLink(gameObject)
+                    .WithCancellation(token)
+            );
+
+            mask.raycastTarget = false;
         }
     }
 }
